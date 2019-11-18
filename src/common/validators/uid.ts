4830b7b941714bd4a9d8321a40c94b85c21d6aa9
KROSF <rodrigosanabria22@gmail.com>
2019-11-18 14:03:55 +0100

    feat: add uid validator

---
+++ src/common/validators/uid.ts
@@ -0,0 +1,19 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments
+} from 'class-validator'
+
+/**
+ * ValidatorConstraintInterface validate if a string has uid format.
+ */
+@ValidatorConstraint()
+export class UID implements ValidatorConstraintInterface {
+  validate (value: string): boolean {
+    return /^u(?:[0-9]{8}|[xyz][0-9]{7})/.test(value)
+  }
+
+  defaultMessage ({ value }: ValidationArguments) {
+    return `${value} has not correct format.`
+  }
+}
