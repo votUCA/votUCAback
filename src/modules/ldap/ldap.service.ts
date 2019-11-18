06caa2db3e7c8d8df2699c2bc7a05471a1980995
KROSF <rodrigosanabria22@gmail.com>
2019-11-18 21:56:45 +0100

    fix: ldap service

--- src/modules/ldap/ldap.service.ts
+++ src/modules/ldap/ldap.service.ts
@@ -1,29 +1,51 @@
-import {
-    Injectable,
-    NotFoundException,
-  } from '@nestjs/common'
+import { Injectable, Inject, NotFoundException } from '@nestjs/common'
 import { Client } from 'ldapts'
 import { ConfigService } from '../config/config.service'
+import { LoginInput } from '../users/login.input'
 
 @Injectable()
 export class LdapService {
-    constructor (
-        @Inject(ConfigService)
-        private readonly configService: ConfigService
-    ){}
-
-    client = new Client({  
-        url: this.configService.ldapHost
+  private readonly client: Client
+  constructor (
+    @Inject(ConfigService)
+    private readonly configService: ConfigService
+  ) {
+    this.client = new Client({
+      url: this.configService.ldapHost
     })
+  }
 
-    async ldap_auth(uid: string,password: string) {
-        try {
-            await this.client.bind('cn=' + uid +',ou=Usuarios,dc=votouca,dc=com', password)
-        } catch {
-            return false
-        } finally {
-            await this.client.unbind()
-            return true
-        }
+  async ldapAuth ({ user, password }: LoginInput) {
+    let isAuth: boolean
+    try {
+      await this.client.bind(
+        `cn=${user},ou=Usuarios,dc=votouca,dc=com`,
+        password
+      )
+      isAuth = true
+    } catch {
+      isAuth = false
+    } finally {
+      await this.client.unbind()
+      // eslint-disable-next-line no-unsafe-finally
+      return isAuth
     }
+  }
+
+  async findByUid (uid: string) {
+    try {
+      const { searchEntries } = await this.client.search(
+        this.configService.ldapBaseDn,
+        {
+          scope: 'sub',
+          filter: `uid=${uid}`
+        }
+      )
+      return searchEntries[0] || null
+    } catch {
+      throw new NotFoundException()
+    } finally {
+      await this.client.unbind()
+    }
+  }
 }
