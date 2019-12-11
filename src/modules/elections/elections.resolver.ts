0fe9084dfa071bfd30b5d42ebc3fb93643fd148a
Iván Castillo Caro <correo@dominio.com>
2019-12-11 22:10:46 +0100

    feat: added voting statistics

--- src/modules/elections/elections.resolver.ts
+++ src/modules/elections/elections.resolver.ts
@@ -5,11 +5,16 @@ import { GqlAuthGuard } from '../auth/gql.guard'
 import { ElectionsService } from './elections.service'
 import { Election } from './elections.type'
 import { ElectionInput } from './elections.input'
+import { CandidatesService } from '../candidates/candidates.service'
+import { AnswersService } from '../answers/answers.service'
+import { ElectionResult } from './results.type'
 
 @Resolver(() => Election)
 @UseGuards(GqlAuthGuard)
 export class ElectionsResolver {
-  constructor (private electionsService: ElectionsService) {}
+  constructor (private electionsService: ElectionsService,
+              private candidatesService: CandidatesService,
+              private answersService: AnswersService) {}
 
   @Query(() => [Election])
   async elections () {
@@ -24,5 +29,14 @@ export class ElectionsResolver {
   @Mutation(() => Election)
   async createElection (@Args('input') input: ElectionInput) {
     return this.electionsService.create(input)
+  }
+
+  @Query(() => [ElectionResult])
+  async showResults (@Args({ name: 'id', type: () => ID }) id: string) {
+    const allCand = await this.candidatesService.findAll({ idElection: id })
+    const result = allCand.reduce((prev, current) => ({ ...prev, [current.id]: 0 }), {})
+    const answers = await this.answersService.findAll({ idCandidate: { $in: Object.keys(result) } })
+    answers.forEach(answer => result[answer.idCandidate]++)
+    return Object.entries(result).map(([id, votos]) => ({ candidate: allCand.find((candidate) => candidate.id === id), votos }))
   }
 }
