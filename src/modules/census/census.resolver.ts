52b11a755190cca8986887dc101c29178c3127c4
Jose Mari <josemari-98@hotmail.com>
2019-12-26 19:04:58 +0100

    Anadido query para censos

--- src/modules/census/census.resolver.ts
+++ src/modules/census/census.resolver.ts
@@ -13,7 +13,7 @@ export class CensusResolver {
   }
 
   @Query(() => [Census])
-  async censues (@Args({ type: () => Int, name: 'limit', defaultValue: 0 }) limit: number, @Args({ type: () => Int, name: 'skip', defaultValue: 0 }) skip: number) {
+  async censuses (@Args({ type: () => Int, name: 'limit', defaultValue: 0 }) limit: number, @Args({ type: () => Int, name: 'skip', defaultValue: 0 }) skip: number) {
     return this.censusService.findAll({ }, { skip, limit })
   }
 }
