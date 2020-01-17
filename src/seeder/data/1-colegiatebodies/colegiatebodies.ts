fd4a348b14ed593dfe8961387395dfef4be596d2
Nieterulz <ajsamu@hotmail.com>
2020-01-18 00:53:46 +0100

    feat: added seeder tests
    Co-authored-by: Rodrigo Sanabria <rodrigosanabria22@gmail.com>

---
+++ src/seeder/data/1-colegiatebodies/colegiatebodies.ts
@@ -0,0 +1,24 @@
+const colegiatebodies = [
+  {
+    _id: '5e1dfd2ff9344483bc3ac1ae',
+    name: 'PNDVP',
+  },
+  {
+    _id: '5e1dfd2ff9344483bc3ac1af',
+    name: 'PDINVP',
+  },
+  {
+    _id: '5e1dfd2ff9344483bc3ac1b0',
+    name: 'PAS',
+  },
+  {
+    _id: '5e1dfd2ff9344483bc3ac1b1',
+    name: 'ALU',
+  },
+  {
+    _id: '5e1dfd2ff9344483bc3ac1ad',
+    name: 'PDVP',
+  },
+]
+
+export = colegiatebodies
